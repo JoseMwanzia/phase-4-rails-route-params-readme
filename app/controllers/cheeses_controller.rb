2e8b616a551df824b7f6f7b56c645ee7ced68d3e
joseph mwanzia <makamwanzia@gmail.com>
2023-03-14 20:23:10 +0300

    Completed lab

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # FIND A CHEESE USING THE ID FROM THE URL
+    cheese = Cheese.find_by(id: params[:id])
+    # SEND A JSON RESPONSE USING THAT CHEESE OBJECT
+    render json: cheese
+
+  end
 end
